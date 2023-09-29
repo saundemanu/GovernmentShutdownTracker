bec7a71b69d5fca126b592bb1ff703e00cd2f683
Emanuel Saunders <cs.emanuel42@gmail.com>
2023-09-29 19:03:20 -0400

    modified to jit mdoe in twconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'tailwindcss'
 import daisyui from 'daisyui';
 
 const config: Config = {
+  mode:'jit',
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
